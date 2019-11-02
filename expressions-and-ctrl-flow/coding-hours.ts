6ec226472e2335794ae7106126b26e40ac2aa2f0
laposih <laposi.helga@gmail.com>
2019-11-02 11:29:37 +0100

    add coding-hours

---
+++ expressions-and-ctrl-flow/coding-hours.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+//
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
+
+let codingHours = 6;
+let weeksOfSemester = 17;
+let averageWorkHours = 52;
+
+console.log(Math.round((codingHours * weeksOfSemester * 5) / (weeksOfSemester * averageWorkHours) * 100));
