932ee635e3599550f53e0ea57a68139ca16c6e94
laposih <laposi.helga@gmail.com>
2019-11-02 10:25:51 +0100

    add hello others task

---
+++ expressions-and-ctrl-flow/hello-others.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+console.log('Hello, Dani!');
+console.log('Hello, Leo!');
+console.log('Hello, Kata!');
