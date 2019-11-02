9a178ad615283ad37779418d0bd77cd6e29b0a05
laposih <laposi.helga@gmail.com>
2019-11-02 16:50:39 +0100

    add print even task

---
+++ expressions-and-ctrl-flow/print-even.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 50
+
+for (let i: number = 0; i <= 50; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+  }
