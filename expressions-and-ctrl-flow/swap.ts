eb1c0cac54639adaa4b03db7ee989fcc1c00703a
laposih <laposi.helga@gmail.com>
2019-11-02 12:04:32 +0100

    add swap task

---
+++ expressions-and-ctrl-flow/swap.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+// Swap the values of these variables
+let a: number = 123;
+let b: number = 526;
+
+console.log(a);
+console.log(b);
+
+/*a = a + b
+b = a - b
+a = a - b*/
+
+let c: number = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
