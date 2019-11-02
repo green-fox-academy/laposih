514ef6d74a1233362b4b3ed826116f383f66d326
laposih <laposi.helga@gmail.com>
2019-11-02 13:50:46 +0100

    add define basic info task

---
+++ expressions-and-ctrl-flow/define-basic-info.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let myName: string = 'Helga';
+let age: number = 30;
+let heightInMeters: number = 1.6;
+let isMarried: boolean = true;
+
+console.log('My name is ' + myName);
+console.log('I am ' + age + ' years old');
+console.log('I am ' + heightInMeters + ' m tall');
+console.log('Am I married? - ' + isMarried);
