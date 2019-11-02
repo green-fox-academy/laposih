2cb4e2500d0078e6f5d280d60893893835f6fdb1
laposih <laposi.helga@gmail.com>
2019-11-02 11:11:02 +0100

    add two numbers task

---
+++ expressions-and-ctrl-flow/two-numbers.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+// Print the result of 13 added to 22
+
+// Print the result of 13 substracted from 22
+
+// Print the result of 22 multiplied by 13
+
+// Print the result of 22 divided by 13 (as a decimal fraction)
+
+// Print the remainder of 22 divided by 13
+
+let firstNumber = 22;
+let secondNumber = 13;
+console.log(firstNumber + secondNumber);
+console.log(firstNumber - secondNumber);
+console.log(firstNumber * secondNumber);
+console.log(firstNumber / secondNumber);
+console.log(firstNumber % secondNumber);
