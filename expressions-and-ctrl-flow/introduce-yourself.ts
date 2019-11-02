e044275e7d4b1419553531d2c8d765f380403eb7
laposih <laposi.helga@gmail.com>
2019-11-02 11:00:40 +0100

    add introduce-yourself task

---
+++ expressions-and-ctrl-flow/introduce-yourself.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
+
+console.log('My name is Helga.');
+console.log('I am ' + 30 + ' years old.');
+console.log('My height in metres is ' + 1.6 + '.');
