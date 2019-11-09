864d75145e17dcebdc821977181219c08e818bcf
laposih <laposi.helga@gmail.com>
2019-11-09 14:23:29 +0100

    add increment elements task

---
+++ arrays-and-functions/increment-element.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// -  Create a variable named `numList` with the following content: `[1, 2, 3, 4, 5]`
+// -  Increment the third element simply by accessing it
+// -  Log the third element to the console
+
+let numList: number[] = [1, 2, 3, 4, 5];
+
+console.log(numList);
+
+numList[2] = numList[2] + 1;
+
+console.log(numList[2]);
+
+
+
+export {}
