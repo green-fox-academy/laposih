d82356e10aac90d1082f3029008943e8c0fd1d41
laposih <laposi.helga@gmail.com>
2019-11-16 12:14:13 +0100

    add print params task

---
+++ arrays-and-functions/print-params.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+function printParams(...params: any[]): any {
+    let output: string = '';
+    for (let i: number = 0; i < params.length; i++) {
+        output = output + params[i] + ' ';
+    }
+    return output;
+    }
+
+console.log(printParams(4, true, "kiskutya"));
+
+export{}
