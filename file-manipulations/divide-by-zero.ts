35c6cdc6a33092218e5cc26485750cc18802a6ab
laposih <laposi.helga@gmail.com>
2019-11-30 11:40:25 +0100

    add divide by zero task

--- file-manipulations/divide-by-zero.ts
+++ file-manipulations/divide-by-zero.ts
@@ -1,5 +1,23 @@
 'use strict';
 
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
 
+function divide(n: number): number {
+    if (n === 0) {
+        throw Error('fail');
+    } else {
+        console.log(10/n);
+        return
+    }
+}
+
+try {
+    divide(0);
+} catch(err) {
+    console.log(err.message);
+}
 
 export{}
